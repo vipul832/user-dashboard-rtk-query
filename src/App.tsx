29ed3ff58745b6f9c357abea896a9222cec2e744
vipulc8 <vipul.c@simformsolutions.com>
2023-05-10 10:51:29 +0530

    Initials Commit for UI

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import UserList from "./components/users/UserList";
+
+//icons import
+import { BsSun } from "react-icons/bs";
+import { MdDarkMode } from "react-icons/md";
+
+//store reducer import
+import { useSelector, useDispatch } from "react-redux";
+import { themeValue, changeTheme } from "./features/theme/themSlice";
+import UserCard from "./components/users/UserCard";
+
+function App() {
+  const theme = useSelector(themeValue);
+  const dispatch = useDispatch();
+  return (
+    <div
+      className={`${
+        theme ? "bg-gray-500" : "bg-indigo-300"
+      } container-lg border min-h-screen justify-center items-center flex py-5 relative`}
+    >
+      <div className=" w-[90%]  bg-[#ffff]/30 backdrop-blur-md rounded-[20px] shadow-lg">
+        <button
+          className="absolute  top-[8px] right-[8px] text-[1rem] rounded-full p-2 z-10 bg-[white]/30 border border-1"
+          onClick={() => dispatch(changeTheme())}
+        >
+          {theme ? <BsSun /> : <MdDarkMode />}
+        </button>
+        <div className="flex lg:justify-evenly py-4 px-4 items-center w-auto h-auto">
+          <UserList />
+          <UserCard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/users/UserCardSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { UserObject } from "../../constant/userData";
+import { RootState } from "../../app/store";
+
+const initialState: UserObject[] = [
+  /*Default value for Testing */
+  // {
+  //   id: "641c3406f8c26a9cb62a4c79",
+  //   first_name: "George",
+  //   last_name: "Bluth",
+  //   email: "george.bluth@reqres.in",
+  //   avatar: "https://reqres.in/img/faces/1-image.jpg",
+  //   active: true,
+  //   owner: true,
+  //   role: "Manager",
+  //   removable: false,
+  // },
+];
+
+const userCardSlice = createSlice({
+  name: "card",
+  initialState,
+  reducers: {
+    mouseEnter: (state, action) => {
+      state.push(action.payload);
+    },
+    mouseLeave: (state) => {
+      state.pop();
+    },
+  },
+});
+
+export const currentUser = (state: RootState) => state.userCard[0];
+
+export const { mouseEnter, mouseLeave } = userCardSlice.actions;
+
+export default userCardSlice.reducer;
